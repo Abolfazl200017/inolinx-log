48927efbf227ff3b541b4dc016081fad77f333df
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-03-27 11:14:48 +0430

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './shared/angular-material/angular-material.module'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularMaterialModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
