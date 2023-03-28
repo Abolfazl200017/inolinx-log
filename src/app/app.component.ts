ec9282d22eb1904e1e77e1dd4ab68e460dd6e78a
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-03-28 15:33:19 +0430

    config scrollbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,15 @@ import { StorageService } from './services/storage/storage.service';
 export class AppComponent implements OnInit{
   darkMode:boolean=false;
   showFiller = false;
-  @ViewChild(Headers) header:ElementRef|undefined;
+  mainHeight:string='';
+  @ViewChild('header') header:ElementRef|undefined;
   constructor(private local:StorageService){}
   ngOnInit():void {
     this.initMode();
+  }
+  ngAfterViewInit():void {
+    this.setMainHeight()
+    console.log(this.mainHeight)
   }
   doIt(){
     console.log('submit search')
@@ -28,5 +33,8 @@ export class AppComponent implements OnInit{
     this.darkMode = !this.darkMode;
     this.local.setData('darkMode', this.darkMode)
     console.log(this.header)
+  }
+  setMainHeight(){
+    this.mainHeight = `height: calc(100% - ${this.header?.nativeElement.offsetHeight}px);`
   }
 }
