3153bea28a8565e9620a5192cc9e745f60c9d9ba
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-03-30 15:30:24 +0430

    config datepicker fields in new project

--- src/app/features/new/project/project.component.ts
+++ src/app/features/new/project/project.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project.component.scss']
 })
 export class ProjectComponent implements OnInit {
-  dateControl:any;
+  startDateControl:any;
+  endDateControl:any;
+  config:any={
+    calendar:'jalali',
+    doneText:'تایید',
+    cancelText:'انصراف',
+    themeConfig: {
+      light: {
+          primaryColor: "#3f51b5", // string
+          secondaryColor: "#444444", // string
+          backgroudColor: "#ffffff" // string
+      },
+      dark: {
+          primaryColor: "#ffffff", //string
+          secondaryColor: "#eeeeee", // string
+          backgroudColor: "#444444" // string
+      },
+      rounded: "medium" // flase | "medium" | "full"
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  log(e:any){
+    console.log(e);
+  }
 }
