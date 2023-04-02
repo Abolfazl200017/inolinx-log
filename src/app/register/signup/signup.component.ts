81db08fae193223a217f6c89869209c79674a464
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-04-02 19:07:45 +0430

    config formgroup to signup

--- src/app/register/signup/signup.component.ts
+++ src/app/register/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, UntypedFormBuilder, UntypedFormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
   passwordVisibility:boolean=false;
-  constructor() { }
-
+  constructor(
+    private formBuilder: UntypedFormBuilder
+  ) { }
+  formGroup = this.formBuilder.group({
+    name: new UntypedFormControl('', [Validators.required]),
+    lastName: new UntypedFormControl('', [Validators.required]),
+    email: new UntypedFormControl('', [Validators.required, Validators.pattern((/^(\d{10}|\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3}))$/))]),
+    password: new UntypedFormControl('', [Validators.required, Validators.minLength(8)]),
+  })
   ngOnInit(): void {
   }
-
+  console(){
+    console.log(this.formGroup.valid)
+  }
 }
