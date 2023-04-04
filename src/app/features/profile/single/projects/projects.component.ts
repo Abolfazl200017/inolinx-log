799317bc9dbfe9c84667b3abc73783f6ae1e015e
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-04-04 19:08:01 +0430

    config project design section in single profile

--- src/app/features/profile/single/projects/projects.component.ts
+++ src/app/features/profile/single/projects/projects.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IProject{
+  image: string;
+  title: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-
+  projects: IProject[]=[
+    {
+      image: 'https://iranich.com/wp-content/uploads/2019/02/file_1042.jpg',
+      title: 'پروژه زومیت',
+      url: ""
+    },
+    {
+      image: 'https://iranich.com/wp-content/uploads/2019/02/file_1042.jpg',
+      title: 'پروژه زومیت',
+      url: ""
+    },
+    {
+      image: 'https://iranich.com/wp-content/uploads/2019/02/file_1042.jpg',
+      title: 'پروژه زومیت',
+      url: ""
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
