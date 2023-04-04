56b0ecc25ec49e7943c96469fbacdbb7990444c1
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-04-04 18:18:38 +0430

    config requests layout section in single profile

--- src/app/features/profile/single/requests/requests.component.ts
+++ src/app/features/profile/single/requests/requests.component.ts
@@ -1,12 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IRequest{
+  name: string;
+  lastName: string;
+  email: string;
+}
+
 @Component({
   selector: 'app-requests',
   templateUrl: './requests.component.html',
   styleUrls: ['./requests.component.scss']
 })
 export class RequestsComponent implements OnInit {
-
+  requests:IRequest[]=[
+    {
+      name: 'ابوالفضل',
+      lastName: 'زراعتکار',
+      email: 'abolfazlzeraatkar@gmail.com'
+    },
+    {
+      name: 'ابوالفضل',
+      lastName: 'زراعتکار',
+      email: 'abolfazlzeraatkar@gmail.com'
+    },
+    {
+      name: 'ابوالفضل',
+      lastName: 'زراعتکار',
+      email: 'abolfazlzeraatkar@gmail.com'
+    },
+    {
+      name: 'ابوالفضل',
+      lastName: 'زراعتکار',
+      email: 'abolfazlzeraatkar@gmail.com'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
