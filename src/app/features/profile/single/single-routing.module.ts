b639a651fda049c68d24f25fbe222bf4cd5305d2
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-04-04 16:32:58 +0430

    make single profile routes

--- src/app/features/profile/single/single-routing.module.ts
+++ src/app/features/profile/single/single-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditComponent } from './edit/edit.component';
+import { IssuesComponent } from './issues/issues.component';
+import { PersonsComponent } from './persons/persons.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { RequestsComponent } from './requests/requests.component';
 import { SingleComponent } from './single.component';
 
-const routes: Routes = [{ path: '', component: SingleComponent }];
+const routes: Routes = [
+  { path: '', component: SingleComponent, children:[
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'issues', component: IssuesComponent},
+    { path: 'projects', component: ProjectsComponent},
+    { path: 'persons', component: PersonsComponent},
+    { path: 'requests', component: RequestsComponent},
+    { path: 'edit', component: EditComponent},
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/profile/single/single.module.ts
+++ src/app/features/profile/single/single.module.ts
@@ -5,12 +5,24 @@ import { SingleRoutingModule } from './single-routing.module';
 import { SingleComponent } from './single.component';
 import { AngularMaterialModule } from 'src/app/shared/angular-material/angular-material.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PersonsComponent } from './persons/persons.component';
+import { RequestsComponent } from './requests/requests.component';
+import { IssuesComponent } from './issues/issues.component';
+import { EditComponent } from './edit/edit.component';
 
 
 @NgModule({
   declarations: [
     SingleComponent,
-    DashboardComponent
+    DashboardComponent,
+    IssuesComponent,
+    ProjectsComponent,
+    PersonsComponent,
+    RequestsComponent,
+    IssuesComponent,
+    RequestsComponent,
+    EditComponent
   ],
   imports: [
     CommonModule,
