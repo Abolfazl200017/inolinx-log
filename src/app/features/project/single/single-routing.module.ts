4d05a3daa1cd6c6d19775376a96c17c6119315fa
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-04-05 16:52:12 +0430

    config project routes

--- src/app/features/project/single/single-routing.module.ts
+++ src/app/features/project/single/single-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { SingleComponent } from '../../profile/single/single.component';
+import { SingleComponent } from './single.component';
+import { IssuesComponent } from './issues/issues.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { EditComponent } from './edit/edit.component';
+import { PersonsComponent } from './persons/persons.component';
 
 const routes: Routes = [
   { path: '', component: SingleComponent, children:[
-    { path: 'dashboard', component: DashboardComponent}
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'issues', component: IssuesComponent},
+    { path: 'categories', component: CategoriesComponent},
+    { path: 'edit', component: EditComponent},
+    { path: 'persons', component: PersonsComponent},
   ] }
 ];
 
--- src/app/features/project/single/single.module.ts
+++ src/app/features/project/single/single.module.ts
@@ -5,12 +5,20 @@ import { SingleRoutingModule } from './single-routing.module';
 import { SingleComponent } from './single.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AngularMaterialModule } from 'src/app/shared/angular-material/angular-material.module';
+import { IssuesComponent } from './issues/issues.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { PersonsComponent } from './persons/persons.component';
+import { EditComponent } from './edit/edit.component';
 
 
 @NgModule({
   declarations: [
     SingleComponent,
     DashboardComponent,
+    IssuesComponent,
+    CategoriesComponent,
+    PersonsComponent,
+    EditComponent,
   ],
   imports: [
     CommonModule,
