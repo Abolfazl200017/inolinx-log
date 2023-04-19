bda589cd9f8f417a5e5bd6bb4d37235d13d83246
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-04-19 13:49:59 +0430

    try to install swiper

--- src/app/features/feed/feed.component.ts
+++ src/app/features/feed/feed.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore, { Navigation } from 'swiper';
 
+SwiperCore.use([Navigation]);
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
@@ -10,6 +12,12 @@ export class FeedComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const swiper = new SwiperCore('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   }
 
 }
