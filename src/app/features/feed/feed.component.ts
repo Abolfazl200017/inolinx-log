390c48c6a472003d49fb8845663569df69ee70e0
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-04-25 19:35:29 +0430

    bug fix

--- src/app/features/feed/feed.component.ts
+++ src/app/features/feed/feed.component.ts
@@ -8,7 +8,7 @@ import { MatPaginator, PageEvent } from '@angular/material/paginator';
 })
 export class FeedComponent implements OnInit {
   pageEvent?:PageEvent;
-  states:string[]=['signup', 'signup', 'signup', 'comment', 'crub', 'comment', 'comment', 'crub', 'crub', 'crub']
+  states:string[]=['signup', 'signup', 'signup', 'comment', 'crud', 'comment', 'comment', 'crud', 'crud', 'crud']
   constructor() { }
 
   ngOnInit(): void {
