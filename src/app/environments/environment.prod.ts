30dc14a812eb317aaf4970ede7d6b65c860b6059
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-04-30 15:36:28 +0430

    config environment

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { resetFakeAsyncZone } from "@angular/core/testing";
+
+export const environment = {
+  production: true,
+  SHARE_PATH : 'http://192.168.149.224:8000',
+  BASE_API_URL : 'http://192.168.149.224:8000/api/',
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  SHARE_PATH : 'http://192.168.149.224:8000',
+  BASE_API_URL : 'http://192.168.149.224:8000/api/',
+};
