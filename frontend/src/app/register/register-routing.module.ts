f8e9aa15fcf8dcd01616994470721346e014eace
zeraatkar <zeraatkar.abolfazl@yahoo.com>
2023-05-07 14:51:33 +0430

    config auth guard in register section

--- frontend/src/app/register/register-routing.module.ts
+++ frontend/src/app/register/register-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register.component';
 import { SignupComponent } from './signup/signup.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', component: RegisterComponent , children:[
-  {path: 'signup', component: SignupComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '', redirectTo: 'login', pathMatch: "full" },
+  {path: 'signup', component: SignupComponent, canActivate:[AuthGuard]},
+  {path: 'login', component: LoginComponent, canActivate:[AuthGuard]},
+  {path: '', redirectTo: 'login', pathMatch: "full"},
 ]}];
 
 @NgModule({
